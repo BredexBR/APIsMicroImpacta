bda4435713f1e12d8355d255903c381527579b86
Breno <breno.ferreira.bre2@exemplo.com>
2025-04-13 17:33:32 -0300

    Tarefa: Realizando o Exercicio 09

--- Parte1/exercicio9.py
+++ Parte1/exercicio9.py
@@ -1,2 +1,5 @@
 from dicionario import dic
 
+# #9 O que o seguinte acesso imprime? Se ele dá erro, qual o erro? Se dá erro, como corrigir?
+print("ex9b", "pudim" in dic["sobremesas"]["doces"])
+#KeyError: 'sobremesas', pois o dicionario dic só tem as chaves "alimentos" e "linguagens"
