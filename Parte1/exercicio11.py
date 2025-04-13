55957d48f67ccc260adcfa3142626db945703451
Breno <breno.ferreira.bre2@exemplo.com>
2025-04-13 17:41:22 -0300

    Tarefa: Realizando o Exercicio 11

--- Parte1/exercicio11.py
+++ Parte1/exercicio11.py
@@ -1,2 +1,14 @@
 from dicionario import dic
 
+#11 Escreva uma função que retorna uma lista (sem repetições) de paradigmas de linguagens de programação
+def todos_paradigmas(dic):
+    lista_linguagens = dic['linguagens']
+    paradigmas = []
+    for linguagem in lista_linguagens:
+        paradigmas_da_linguagem = linguagem['paradigma']
+        for p in paradigmas_da_linguagem:
+            if p not in paradigmas:
+                paradigmas.append(p)
+    return paradigmas 
+
+print(todos_paradigmas(dic))
