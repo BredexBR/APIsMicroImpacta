74840b0dbe8ed65b445168c7d3e5e33f8148a9cf
Breno <breno.ferreira.bre2@exemplo.com>
2025-04-13 17:32:20 -0300

    Tarefa: Realizando o Exercicio 08

--- Parte1/exercicio7.py
+++ Parte1/exercicio7.py
@@ -3,7 +3,7 @@ from dicionario import dic
 # #7 O que o seguinte acesso imprime? Se ele dá erro, qual o erro? Se dá erro, como corrigir?
 print("r7", dic["alimentos"]["pizzas"][2] == "mussarella")
 # False
-# dic["alimentos"]["pizzas"] é a lista ["margueritta", "mussarella",                    "frango com catupiry", "portuguesa"]
+# dic["alimentos"]["pizzas"] é a lista ["margueritta", "mussarella", "frango com catupiry", "portuguesa"]
 # A sua posição 0 é "margueritta"
 # A sua posição 1 é "mussarella"
 # A sua posição 2 é "frango com catupiry"
--- Parte1/exercicio8.py
+++ Parte1/exercicio8.py
@@ -1,2 +1,7 @@
 from dicionario import dic
 
+# #8 O que o seguinte acesso imprime? Se ele dá erro, qual o erro? Se dá erro, como corrigir?
+print("r8", 1996 in dic['linguagens'][0])
+#False, pois 1996 não é uma **chave** do dicionário dic['linguagens'][0]
+#Alias, dic['linguagens'][0] é  {"nome": "javascript", "criacao": 1996, 
+#        "paradigma": ["eventos","funcional"]}
