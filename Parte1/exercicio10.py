510aad449153e8087faeaae628e404862360142b
Breno <breno.ferreira.bre2@exemplo.com>
2025-04-13 17:35:38 -0300

    Tarefa: Realizando o Exercicio 10

--- Parte1/exercicio10.py
+++ Parte1/exercicio10.py
@@ -1,2 +1,12 @@
 from dicionario import dic
 
+#10 Escreva uma função "mais velha" que 
+# recebe um dicionário como dic e 
+# retorna (isso é diferente de imprimir!) a linguagem de programação mais velha da nossa lista
+def mais_velha(dic):
+    lista_linguagens = dic['linguagens']
+    ling_velha = lista_linguagens[0]
+    for linguagem in lista_linguagens:
+        if linguagem['criacao'] < ling_velha['criacao']:
+            ling_velha = linguagem
+    return ling_velha
