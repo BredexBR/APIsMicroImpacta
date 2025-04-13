11d82195efd3317f6c50a58952d365632bac3e11
Breno <breno.ferreira.bre2@exemplo.com>
2025-04-13 17:26:44 -0300

    Tarefa: Realizando o Exercicio 06

--- Parte1/exercicio6.py
+++ Parte1/exercicio6.py
@@ -1,2 +1,7 @@
 from dicionario import dic
 
+# #6 O que o seguinte acesso imprime? Se ele dá erro, qual o erro? Se dá erro, como corrigir?
+print("r6", dic["linguagens"][2] == "haskell")
+# False
+# dic["linguagens"][2] é o dicionário         {"nome": "haskell", "criacao": 1990, "paradigma": ["funcional"]}
+# Esse dicionário não é a mesma coisa que a string "haskell"
