c9693d2f87507feaae2ec2d88e1dedd926fdb2d9
Breno <breno.ferreira.bre2@exemplo.com>
2025-04-13 14:09:54 -0300

    Tarefa: Preparando ambiente para inserção dos exercicios

---
+++ Parte1/dicionario.py
@@ -0,0 +1,21 @@
+dic = {
+    "alimentos": {
+        "pizzas": ["margueritta", "mussarella", 
+                   "frango com catupiry", "portuguesa"],
+        "bolos": ("floresta negra", 
+                   "red velvet", 
+                   "de laranja", "dá vó"),
+        "calorias": {
+            "leite": 129, "fatia pizza": 320,
+            "agua": 0, "maça": 95
+            }
+    },
+    "linguagens": [
+        {"nome": "javascript", "criacao": 1996, 
+        "paradigma": ["eventos","funcional"]},
+        {"nome": "python", "criacao": 1991, 
+        "paradigma": ["orientada a objetos","estruturada"]},
+        {"nome": "haskell", "criacao": 1990, 
+        "paradigma": ["funcional"]}
+        ]
+    }
---
+++ Parte1/exercicio3.py
@@ -0,0 +1,2 @@
+from dicionario import dic
+
