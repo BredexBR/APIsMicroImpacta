969d43cdaf15ca76d93085414f387bc10a995495
Breno <breno.ferreira.bre2@exemplo.com>
2025-04-13 17:24:46 -0300

    Tarefa: Realizando o Exercicio 05

--- Parte1/exercicio5.py
+++ Parte1/exercicio5.py
@@ -1,2 +1,7 @@
 from dicionario import dic
 
+# #5. O que o seguinte acesso imprime? Se ele dá erro, qual o erro? Se dá erro, como corrigir?
+print("r5", dic["linguagens"]["javascript"]["criacao"])
+# TypeError: list indices must be integers or slices, not str
+# O erro ocorre porque dic["linguagens"] é uma lista
+# O acesso correto seria dic["linguagens"][0] em vez de dic["linguagens"]["javascript"]
