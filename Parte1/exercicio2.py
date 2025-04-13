41a76cdeaa5d962cc92dd63f982df2bdf9a9b23f
Breno <breno.ferreira.bre2@exemplo.com>
2025-04-13 17:19:30 -0300

    Tarefa: Realizando o Exercicio 02

--- Parte1/exercicio2.py
+++ Parte1/exercicio2.py
@@ -1,2 +1,5 @@
 from dicionario import dic
 
+# #2. dic['linguagens'] é uma tupla, um dicionário ou uma lista?
+print("r2", type(dic['linguagens']))
+# É uma lista, repare nos colchetes inicial e final
